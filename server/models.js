d44c7fd828e11a3691d9b94fb7a6a8121a7f77ee
Nicholas Forster <nicholas.forster2@griffithuni.edu.au>
2024-10-04 01:04:05 -0700

    model and socket changes

--- server/models.js
+++ server/models.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const User = require('./user.model');
+
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -6,6 +8,8 @@ const userSchema = new mongoose.Schema({
   roles: [String],
   groups: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Group' }],
 });
+
+const User = mongoose.model('User', userSchema);
 
 const groupSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -18,8 +22,26 @@ const channelSchema = new mongoose.Schema({
   group: { type: mongoose.Schema.Types.ObjectId, ref: 'Group' },
 });
 
+const messageSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  message: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+  channel: { type: mongoose.Schema.Types.ObjectId, ref: 'Channel' },
+});
+
 module.exports = {
   User: mongoose.model('User', userSchema),
   Group: mongoose.model('Group', groupSchema),
   Channel: mongoose.model('Channel', channelSchema),
+  Message: mongoose.model('Message', messageSchema),
+  registerUser: (user) => {
+    const newUser = new User(user);
+    newUser.save((err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('User registered:', user.username);
+      }
+    });
+  }
 };
--- server/sockets.js
+++ server/sockets.js
@@ -1,4 +1,7 @@
 const { Server } = require('socket.io');
+const mongoose = require('mongoose');
+const Message = require('./message.model');
+const UserService = require('./user.service');
 
 module.exports = (server) => {
   const io = new Server(server, {
@@ -13,9 +16,24 @@ module.exports = (server) => {
 
     // Handle messages sent by clients
     socket.on('chat message', (msg) => {
-      console.log('Message received:', msg);
-      io.emit('chat message', msg);  // Broadcast message to all clients
-    });
+        console.log('Received message:', msg);
+        const message = new Message({
+          username: msg.username,
+          message: msg.message,
+        });
+        Message.create({
+            username: msg.username,
+            message: msg.message,
+          }, (err, savedMessage) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log('Message saved to database:', savedMessage);
+              console.log('Message ID:', savedMessage._id);
+              io.emit('chat message', msg);  // Broadcast message to all clients
+            }
+        });
+      });
 
     socket.on('disconnect', () => {
       console.log('A user disconnected:', socket.id);
