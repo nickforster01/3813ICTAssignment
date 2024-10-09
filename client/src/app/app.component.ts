f8f2c5baa712f8a58a482f85be2644b5fdb621e1
Nicholas Forster <nicholas.forster2@griffithuni.edu.au>
2024-10-09 05:30:10 -0700

    Chatfunction setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -833,6 +833,13 @@ getMessages(): Observable<any> {
   }
 }
 
+openChatPage() {
+  // Open the localhost:3001 URL in a new pop-up window
+  window.open('http://localhost:3001', '_blank', 'width=800,height=600');
+  // Navigate to the 'chat' section in the main page
+  this.navigateTo('chat');
+}
+
 fetchGroups() {
   this.http.get<Group[]>(`http://localhost:3000/api/groups`).subscribe(
     (data: Group[]) => {
